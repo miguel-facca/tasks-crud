aae444124e4a2ed6a13535b91ec05deff33d3669
Miguel Facca <79928127+snowzitcho@users.noreply.github.com>
2024-08-21 14:06:33 -0300

    feat: setup frontend

---
+++ fe/src/app.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./components/header";
+
+export function App() {
+  return (
+    <div className="w-screen h-screen bg-black flex flex-col">
+      <Header />
+    </div>
+  );
+}
---
+++ fe/src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="h-16 border-2 border-solid border-white/15 flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">Facca Tasks</h1>
+    </header>
+  );
+}
